fa05de6ecb28a11145b67afe5c46b6608eecb6f9
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-01-12 20:24:07 +0900

    [Feat] 컬러 에셋 추가, extension 작성

---
+++ TnT/Projects/DesignSystem/Sources/Color+DesignSystem.swift
@@ -0,0 +1,134 @@
+//
+//  Color+DesignSystem.swift
+//  DesignSystem
+//
+//  Created by 박민서 on 1/12/25.
+//  Copyright © 2025 yapp25-app2team. All rights reserved.
+//
+
+import SwiftUI
+
+// MARK: Sementic Color
+extension Color {
+    // App Color
+    static let accentColor = DesignSystemAsset.accentColor.swiftUIColor
+}
+
+// MARK: Gray Scale
+extension Color {
+    // Common
+    static let common0 = DesignSystemAsset.common0.swiftUIColor
+    static let common100 = DesignSystemAsset.common100.swiftUIColor
+    // Neutral
+    static let neutral50 = DesignSystemAsset.neutral50.swiftUIColor
+    static let neutral100 = DesignSystemAsset.neutral100.swiftUIColor
+    static let neutral200 = DesignSystemAsset.neutral200.swiftUIColor
+    static let neutral300 = DesignSystemAsset.neutral300.swiftUIColor
+    static let neutral400 = DesignSystemAsset.neutral400.swiftUIColor
+    static let neutral500 = DesignSystemAsset.neutral500.swiftUIColor
+    static let neutral600 = DesignSystemAsset.neutral600.swiftUIColor
+    static let neutral700 = DesignSystemAsset.neutral700.swiftUIColor
+    static let neutral800 = DesignSystemAsset.neutral800.swiftUIColor
+    static let neutral900 = DesignSystemAsset.neutral900.swiftUIColor
+    static let neutral950 = DesignSystemAsset.neutral950.swiftUIColor
+}
+
+// MARK: Color Scale
+extension Color {
+    // Blue
+    static let blue50 = DesignSystemAsset.blue50.swiftUIColor
+    static let blue100 = DesignSystemAsset.blue100.swiftUIColor
+    static let blue200 = DesignSystemAsset.blue200.swiftUIColor
+    static let blue300 = DesignSystemAsset.blue300.swiftUIColor
+    static let blue400 = DesignSystemAsset.blue400.swiftUIColor
+    static let blue500 = DesignSystemAsset.blue500.swiftUIColor
+    static let blue600 = DesignSystemAsset.blue600.swiftUIColor
+    static let blue700 = DesignSystemAsset.blue700.swiftUIColor
+    static let blue800 = DesignSystemAsset.blue800.swiftUIColor
+    static let blue900 = DesignSystemAsset.blue900.swiftUIColor
+    static let blue950 = DesignSystemAsset.blue950.swiftUIColor
+    // Green
+    static let green50 = DesignSystemAsset.green50.swiftUIColor
+    static let green100 = DesignSystemAsset.green100.swiftUIColor
+    static let green200 = DesignSystemAsset.green200.swiftUIColor
+    static let green300 = DesignSystemAsset.green300.swiftUIColor
+    static let green400 = DesignSystemAsset.green400.swiftUIColor
+    static let green500 = DesignSystemAsset.green500.swiftUIColor
+    static let green600 = DesignSystemAsset.green600.swiftUIColor
+    static let green700 = DesignSystemAsset.green700.swiftUIColor
+    static let green800 = DesignSystemAsset.green800.swiftUIColor
+    static let green900 = DesignSystemAsset.green900.swiftUIColor
+    static let green950 = DesignSystemAsset.green950.swiftUIColor
+    // Lime
+    static let lime50 = DesignSystemAsset.lime50.swiftUIColor
+    static let lime100 = DesignSystemAsset.lime100.swiftUIColor
+    static let lime200 = DesignSystemAsset.lime200.swiftUIColor
+    static let lime300 = DesignSystemAsset.lime300.swiftUIColor
+    static let lime400 = DesignSystemAsset.lime400.swiftUIColor
+    static let lime500 = DesignSystemAsset.lime500.swiftUIColor
+    static let lime600 = DesignSystemAsset.lime600.swiftUIColor
+    static let lime700 = DesignSystemAsset.lime700.swiftUIColor
+    static let lime800 = DesignSystemAsset.lime800.swiftUIColor
+    static let lime900 = DesignSystemAsset.lime900.swiftUIColor
+    static let lime950 = DesignSystemAsset.lime950.swiftUIColor
+    // Orange
+    static let orange50 = DesignSystemAsset.orange50.swiftUIColor
+    static let orange100 = DesignSystemAsset.orange100.swiftUIColor
+    static let orange200 = DesignSystemAsset.orange200.swiftUIColor
+    static let orange300 = DesignSystemAsset.orange300.swiftUIColor
+    static let orange400 = DesignSystemAsset.orange400.swiftUIColor
+    static let orange500 = DesignSystemAsset.orange500.swiftUIColor
+    static let orange600 = DesignSystemAsset.orange600.swiftUIColor
+    static let orange700 = DesignSystemAsset.orange700.swiftUIColor
+    static let orange800 = DesignSystemAsset.orange800.swiftUIColor
+    static let orange900 = DesignSystemAsset.orange900.swiftUIColor
+    static let orange950 = DesignSystemAsset.orange950.swiftUIColor
+    // Pink
+    static let pink50 = DesignSystemAsset.pink50.swiftUIColor
+    static let pink100 = DesignSystemAsset.pink100.swiftUIColor
+    static let pink200 = DesignSystemAsset.pink200.swiftUIColor
+    static let pink300 = DesignSystemAsset.pink300.swiftUIColor
+    static let pink400 = DesignSystemAsset.pink400.swiftUIColor
+    static let pink500 = DesignSystemAsset.pink500.swiftUIColor
+    static let pink600 = DesignSystemAsset.pink600.swiftUIColor
+    static let pink700 = DesignSystemAsset.pink700.swiftUIColor
+    static let pink800 = DesignSystemAsset.pink800.swiftUIColor
+    static let pink900 = DesignSystemAsset.pink900.swiftUIColor
+    static let pink950 = DesignSystemAsset.pink950.swiftUIColor
+    // Purple
+    static let purple50 = DesignSystemAsset.purple50.swiftUIColor
+    static let purple100 = DesignSystemAsset.purple100.swiftUIColor
+    static let purple200 = DesignSystemAsset.purple200.swiftUIColor
+    static let purple300 = DesignSystemAsset.purple300.swiftUIColor
+    static let purple400 = DesignSystemAsset.purple400.swiftUIColor
+    static let purple500 = DesignSystemAsset.purple500.swiftUIColor
+    static let purple600 = DesignSystemAsset.purple600.swiftUIColor
+    static let purple700 = DesignSystemAsset.purple700.swiftUIColor
+    static let purple800 = DesignSystemAsset.purple800.swiftUIColor
+    static let purple900 = DesignSystemAsset.purple900.swiftUIColor
+    static let purple950 = DesignSystemAsset.purple950.swiftUIColor
+    // Red
+    static let red50 = DesignSystemAsset.red50.swiftUIColor
+    static let red100 = DesignSystemAsset.red100.swiftUIColor
+    static let red200 = DesignSystemAsset.red200.swiftUIColor
+    static let red300 = DesignSystemAsset.red300.swiftUIColor
+    static let red400 = DesignSystemAsset.red400.swiftUIColor
+    static let red500 = DesignSystemAsset.red500.swiftUIColor
+    static let red600 = DesignSystemAsset.red600.swiftUIColor
+    static let red700 = DesignSystemAsset.red700.swiftUIColor
+    static let red800 = DesignSystemAsset.red800.swiftUIColor
+    static let red900 = DesignSystemAsset.red900.swiftUIColor
+    static let red950 = DesignSystemAsset.red950.swiftUIColor
+    // Violet
+    static let violet50 = DesignSystemAsset.violet50.swiftUIColor
+    static let violet100 = DesignSystemAsset.violet100.swiftUIColor
+    static let violet200 = DesignSystemAsset.violet200.swiftUIColor
+    static let violet300 = DesignSystemAsset.violet300.swiftUIColor
+    static let violet400 = DesignSystemAsset.violet400.swiftUIColor
+    static let violet500 = DesignSystemAsset.violet500.swiftUIColor
+    static let violet600 = DesignSystemAsset.violet600.swiftUIColor
+    static let violet700 = DesignSystemAsset.violet700.swiftUIColor
+    static let violet800 = DesignSystemAsset.violet800.swiftUIColor
+    static let violet900 = DesignSystemAsset.violet900.swiftUIColor
+    static let violet950 = DesignSystemAsset.violet950.swiftUIColor
+}
