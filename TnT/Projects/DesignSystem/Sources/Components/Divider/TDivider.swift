b9f1fbe21d6fba30f91000dc6446ee64527f94d1
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-01-14 22:19:26 +0900

    TDivider 구현

--- TnT/Projects/DesignSystem/Sources/Components/Divider/TDivider.swift
+++ TnT/Projects/DesignSystem/Sources/Components/Divider/TDivider.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2025 yapp25thTeamTnT. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+/// TnT 앱 내에서 전반적으로 사용되는 커스텀 디바이더입니다
+public struct TDivider: View {
+    /// Divider 높이
+    let height: CGFloat
+    /// Divider 컬러
+    let color: Color
+    
+    /// - Parameters:
+    ///   - height: Divider의 두께 (기본값: `1`)
+    ///   - color: Divider의 색상
+    public init(height: CGFloat = 1, color: Color) {
+        self.height = height
+        self.color = color
+    }
+    
+    public var body: some View {
+        Rectangle()
+            .fill(color)
+            .frame(height: height)
+    }
+}
