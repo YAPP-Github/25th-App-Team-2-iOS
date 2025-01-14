6b9e7dc3786d5f7ee381ac4b8a99ed9626b95a07
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-01-15 07:44:47 +0900

    [Feat] TTextField Header, Footer 분리, ViewModifier 작성

--- TnT/Projects/DesignSystem/Sources/Components/TextField/TTextField.swift
+++ TnT/Projects/DesignSystem/Sources/Components/TextField/TTextField.swift
@@ -11,113 +11,161 @@ import SwiftUI
 /// TnT 앱 내에서 전반적으로 사용되는 커스텀 텍스트 필드 컴포넌트입니다.
 public struct TTextField: View {
     
+    /// 텍스트 필드 우측에 표시될 RightView
+    private let rightView: RightView?
     /// Placeholder 텍스트
     private let placeholder: String
-    /// 상단 헤더 설정
-    private let header: HeaderContent?
-    /// 하단 푸터 텍스트
-    private let footerText: String?
-    /// 우측 단위 텍스트
-    private let unitText: String?
-    /// 우측 버튼 설정
-    private let button: ButtonContent?
-    /// 키보드 타입
-    private let keyboardType: UIKeyboardType
     
     /// 입력 텍스트
     @Binding private var text: String
     /// 텍스트 필드 상태
     @Binding private var status: Status
+    /// 텍스트 필드 포커스 상태
+    @FocusState var isFocused: Bool
     
     /// - Parameters:
     ///   - placeholder: Placeholder 텍스트 (기본값: "내용을 입력해주세요")
-    ///   - header: 상단 헤더 설정 (옵션)
-    ///   - footerText: 하단 푸터 텍스트 (옵션)
-    ///   - unitText: 우측 단위 텍스트 (옵션)
-    ///   - button: 우측 버튼 설정 (옵션)
-    ///   - keyboardType: 텍스트 필드 키보드 타입 (기본값: .default)
     ///   - text: 입력 텍스트 (Binding)
     ///   - textFieldStatus: 텍스트 필드 상태 (Binding)
+    ///   - rightView: 텍스트 필드 우측에 표시될 `TTextField.RightView`를 정의하는 클로저.
     public init(
         placeholder: String = "내용을 입력해주세요",
-        header: HeaderContent? = nil,
-        footerText: String? = nil,
-        unitText: String? = nil,
-        button: ButtonContent? = nil,
-        keyboardType: UIKeyboardType = .default,
         text: Binding<String>,
-        textFieldStatus: Binding<Status>
+        textFieldStatus: Binding<Status>,
+        @ViewBuilder rightView: () -> RightView? = { nil }
     ) {
         self.placeholder = placeholder
-        self.header = header
-        self.footerText = footerText
-        self.unitText = unitText
-        self.button = button
-        self.keyboardType = keyboardType
         self._text = text
         self._status = textFieldStatus
+        self.rightView = rightView()
     }
-
+    
     public var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            // Header
-            if let header {
-                HStack(spacing: 0) {
-                    Text(header.title)
-                        .typographyStyle(.body1Bold, with: .neutral900)
-                    if header.isRequired {
-                        Text("*")
-                            .typographyStyle(.body1Bold, with: .red500)
-                    }
-                    
-                    Spacer()
-                    
-                    if let limitCount = header.limitCount {
-                        Text("\(text.count)/\(limitCount)자")
-                            .typographyStyle(.label1Medium, with: .neutral400)
-                            .padding(.horizontal, 4)
-                            .padding(.vertical, 2)
-                    }
+        // Text Field
+        VStack(spacing: 0) {
+            HStack(spacing: 0) {
+                TextField(placeholder, text: $text)
+                    .autocorrectionDisabled()
+                    .focused($isFocused)
+                    .font(Typography.FontStyle.body1Medium.font)
+                    .lineSpacing(Typography.FontStyle.body1Medium.lineSpacing)
+                    .kerning(Typography.FontStyle.body1Medium.letterSpacing)
+                    .tint(Color.neutral800)
+                    .foregroundStyle(status.textColor)
+                    .padding(8)
+                    .frame(height: 42)
+                
+                if let rightView {
+                    rightView
                 }
             }
             
-            // Text Field
-            VStack(spacing: 0) {
-                HStack(spacing: 0) {
-                    TextField(placeholder, text: $text)
-                        .keyboardType(keyboardType)
-                        .multilineTextAlignment(unitText != nil ? .trailing : .leading)
-                        .padding(8)
+            TDivider(color: status.underlineColor(isFocused: isFocused))
+        }
+    }
+}
 
-                    if let unitText {
-                        Text(unitText)
-                            .typographyStyle(.body1Medium, with: status.style.textColor)
-                            .padding(.horizontal, 12)
-                            .padding(.vertical, 3)
-                    }
-                    
-                    if let button {
-                        // TODO: 추후 버튼 컴포넌트 나오면 대체
-                        Button(action: button.tapAction ?? { print("Button tapped") }) {
-                            Text(button.title)
-                                .typographyStyle(.label2Medium, with: .neutral50)
-                                .padding(.horizontal, 12)
-                                .padding(.vertical, 7)
-                                .background(Color.neutral900)
-                                .clipShape(.rect(cornerRadius: 8))
-                        }
-                        .padding(.vertical, 5)
-                    }
+public extension TTextField.RightView {
+    ///
+    enum Style {
+        case unit(text: String, status: TTextField.Status)
+        case button(title: String, tapAction: () -> Void)
+    }
+}
+
+public extension TTextField {
+    /// TextField 우측 컨텐츠 뷰입니다
+    struct RightView: View {
+        /// 컨텐츠 스타일
+        private let style: RightView.Style
+        
+        public init(style: RightView.Style) {
+            self.style = style
+        }
+        
+        public var body: some View {
+            
+            switch style {
+            case let .unit(text, status):
+                Text(text)
+                    .typographyStyle(.body1Medium, with: status.textColor)
+                    .padding(.horizontal, 12)
+                    .padding(.vertical, 3)
+                
+            case let .button(title, tapAction):
+                // TODO: 추후 버튼 컴포넌트 나오면 대체
+                Button(action: tapAction) {
+                    Text(title)
+                        .typographyStyle(.label2Medium, with: .neutral50)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 7)
+                        .background(Color.neutral900)
+                        .clipShape(.rect(cornerRadius: 8))
+                }
+                .padding(.vertical, 5)
+            }
+        }
+    }
+    
+    /// TextField 상단 헤더입니다
+    struct Header: View {
+        /// 필수 여부를 표시
+        private let isRequired: Bool
+        /// 헤더의 제목
+        private let title: String
+        /// 입력 가능한 글자 수 제한
+        private let limitCount: Int?
+        /// 입력된 텍스트
+        @Binding private var text: String
+        
+        public init(
+            isRequired: Bool,
+            title: String,
+            limitCount: Int?,
+            text: Binding<String>
+        ) {
+            self.isRequired = isRequired
+            self.title = title
+            self.limitCount = limitCount
+            self._text = text
+        }
+        
+        public var body: some View {
+            HStack(spacing: 0) {
+                Text(title)
+                    .typographyStyle(.body1Bold, with: .neutral900)
+                if isRequired {
+                    Text("*")
+                        .typographyStyle(.body1Bold, with: .red500)
                 }
                 
-                TDivider(color: status.style.statusColor)
+                Spacer()
+                
+                if let limitCount {
+                    Text("\(text.count)/\(limitCount)자")
+                        .typographyStyle(.label1Medium, with: .neutral400)
+                        .padding(.horizontal, 4)
+                        .padding(.vertical, 2)
+                }
             }
-            
-            // Footer
-            if let footer = footerText {
-                Text(footer)
-                    .typographyStyle(.body2Medium, with: status.style.statusColor)
-            }
+        }
+    }
+    
+    /// TextField 하단 푸터입니다
+    struct Footer: View {
+        /// 푸터 텍스트
+        private let footerText: String
+        /// 텍스트 필드 상태
+        @Binding private var status: Status
+        
+        public init(footerText: String, status: Binding<Status>) {
+            self.footerText = footerText
+            self._status = status
+        }
+        
+        public var body: some View {
+            Text(footerText)
+                .typographyStyle(.body2Medium, with: status.footerColor)
         }
     }
 }
@@ -126,54 +174,85 @@ public extension TTextField {
     /// TextField에 표시되는 상태입니다
     enum Status {
         case empty
-        case focused
+        case filled
         case invalid
         case valid
-        case filled
         
-        /// Status에 따른 컬러웨이입니다
-        var style: (statusColor: Color, textColor: Color) {
+        /// 밑선 색상 설정
+        func underlineColor(isFocused: Bool) -> Color {
+            switch self {
+            case .empty:
+                return isFocused ? .neutral600 : .neutral200
+            case .filled:
+                return isFocused ? .neutral600 : .neutral200
+            case .invalid:
+                return .red500
+            case .valid:
+                return .blue500
+            }
+        }
+        
+        /// 텍스트 색상 설정
+        var textColor: Color {
+            switch self {
+            case .empty:
+                return .neutral400
+            case .filled, .invalid, .valid:
+                return .neutral600
+            }
+        }
+        
+        /// 푸터 색상 설정
+        var footerColor: Color {
             switch self {
             case .empty, .filled:
-                return (.neutral200, .neutral400)
-            case .focused:
-                return (.neutral600, .neutral600)
+                return .clear
             case .invalid:
-                return (.red500, .neutral600)
+                return .red500
             case .valid:
-                return (.blue500, .neutral600)
+                return .blue500
+            }
+        }
+    }
+}
+
+struct TTextFieldModifier: ViewModifier {
+    /// Textfield 상단에 표시될 헤더
+    private let header: TTextField.Header?
+    /// Textfield 하단에 표시될 푸터
+    private let footer: TTextField.Footer?
+    
+    public init(header: TTextField.Header?, footer: TTextField.Footer?) {
+        self.header = header
+        self.footer = footer
+    }
+    
+    func body(content: Content) -> some View {
+        VStack(alignment: .leading, spacing: 8) {
+            // 헤더 추가
+            if let header {
+                header
+            }
+            
+            // 본체(TextField)
+            content
+            
+            // 푸터 추가
+            if let footer {
+                footer
             }
         }
     }
 }
 
 public extension TTextField {
-    /// TextField 상단 헤더 설정입니다
-    struct HeaderContent {
-        /// 필수 여부를 표시
-        let isRequired: Bool
-        /// 헤더의 제목
-        let title: String
-        /// 입력 가능한 글자 수 제한
-        let limitCount: Int?
-        
-        public init(isRequired: Bool, title: String, limitCount: Int?) {
-            self.isRequired = isRequired
-            self.title = title
-            self.limitCount = limitCount
-        }
-    }
-    
-    /// TextField 우측 버튼 설정입니다
-    struct ButtonContent {
-        /// 버튼에 표시될 텍스트
-        let title: String
-        /// 버튼 클릭 시 실행되는 동작
-        let tapAction: (() -> Void)?
-        
-        public init(title: String, tapAction: (() -> Void)?) {
-            self.title = title
-            self.tapAction = tapAction
-        }
+    /// 헤더와 푸터를 포함한 레이아웃을 텍스트 필드에 적용합니다.
+    ///
+    /// - Parameters:
+    ///   - header: `TTextField.Header`로 정의된 상단 헤더. (옵션)
+    ///   - footer: `TTextField.Footer`로 정의된 하단 푸터. (옵션)
+    /// - Returns: 헤더와 푸터가 포함된 새로운 View.
+    func withSectionLayout(header: TTextField.Header? = nil, footer: TTextField.Footer? = nil) -> some View {
+        self.modifier(TTextFieldModifier(header: header, footer: footer))
     }
 }
