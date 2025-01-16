4584e948b03a170e215bd331f1f7a96a936efeb1
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-01-17 07:17:19 +0900

    [Feat] UserTypeSelection 화면 작성

--- TnT/Projects/Presentation/Sources/Onboarding/UserTypeSelection/UserTypeSelectionFeature.swift
+++ TnT/Projects/Presentation/Sources/Onboarding/UserTypeSelection/UserTypeSelectionFeature.swift
@@ -6,4 +6,76 @@
 //  Copyright © 2025 yapp25thTeamTnT. All rights reserved.
 //
 
-import Foundation
+import ComposableArchitecture
+
+import Domain
+
+/// 역할 선택 화면의 상태 및 로직을 관리하는 리듀서입니다.
+@Reducer
+public struct UserTypeSelectionFeature {
+    
+    @ObservableState
+    public struct State {
+        /// 현재 선택된 유저 타입 (트레이너/트레이니)
+        var userType: UserType
+        /// UI 관련 상태
+        var viewState: ViewState
+        
+        /// `UserTypeSelectionFeature.State`의 생성자
+        /// - Parameters:
+        ///   - userType: 현재 선택된 유저 타입 (기본값: `.trainer`)
+        ///   - viewState: UI 관련 상태 (기본값: `ViewState()`).
+        public init(userType: UserType = .trainer, viewState: ViewState = .init()) {
+            self.userType = userType
+            self.viewState = viewState
+        }
+    }
+    
+    /// UI 관련 상태를 관리하는 구조체입니다.
+    public struct ViewState: Equatable {
+        /// 다음 화면으로 이동 여부
+        public var isNavigating: Bool
+        
+        /// `ViewState`의 생성자
+        /// - Parameter isNavigating: 네비게이션 여부 (기본값: `false`)
+        public init(isNavigating: Bool = false) {
+            self.isNavigating = isNavigating
+        }
+    }
+    
+    
+    public enum Action: ViewAction {
+        /// 네비게이션 여부 설정
+        case setNavigating(Bool)
+        /// 뷰에서 발생한 액션을 처리합니다.
+        case view(ViewAction)
+        
+        public enum ViewAction {
+            /// 유저 타입 선택 버튼이 눌렸을 때
+            case tapUserTypeButton(UserType)
+            /// "다음으로" 버튼이 눌렸을 때
+            case tapNextButton
+        }
+    }
+    
+    public init() {}
+    
+    public var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .view(let action):
+                switch action {
+                case .tapUserTypeButton(let userType):
+                    state.userType = userType
+                    return .none
+                case .tapNextButton:
+                    print("다음으로..")
+                    return .send(.setNavigating(true))
+                }
+            case .setNavigating(let isNavigating):
+                state.viewState.isNavigating = isNavigating
+                return .none
+            }
+        }
+    }
+}
--- TnT/Projects/Presentation/Sources/Onboarding/UserTypeSelection/UserTypeSelectionView.swift
+++ TnT/Projects/Presentation/Sources/Onboarding/UserTypeSelection/UserTypeSelectionView.swift
@@ -9,8 +9,136 @@
 import SwiftUI
 import ComposableArchitecture
 
+import Domain
 import DesignSystem
 
+/// 역할 선택 화면을 담당하는 View입니다.
+@ViewAction(for: UserTypeSelectionFeature.self)
 public struct UserTypeSelectionView: View {
     
+    public var store: StoreOf<UserTypeSelectionFeature>
+    
+    /// `UserTypeSelectionView`의 생성자
+    /// - Parameter store: `UserTypeSelectionFeature`의 상태를 관리하는 `Store`
+    public init(store: StoreOf<UserTypeSelectionFeature>) {
+        self.store = store
+    }
+    
+    public var body: some View {
+        NavigationStack {
+            VStack {
+                
+                Spacer(minLength: 60)
+                
+                VStack(spacing: 48) {
+                    Header
+                    
+                    ImageSection
+                    
+                    ButtonSection
+                }
+                
+                Spacer()
+                
+                BottomTempButton {
+                    send(.tapNextButton)
+                }
+            }
+            .ignoresSafeArea(.container, edges: .bottom)
+            .navigationDestination(
+                isPresented: Binding(get: { store.viewState.isNavigating }, set: { store.send(.setNavigating($0))})
+            ) {
+                DummyView()
+            }
+        }
+    }
+}
+
+// MARK: - SubViews
+private extension UserTypeSelectionView {
+    var Header: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text("안녕하세요!\n어떤 회원으로 이용하시겠어요?")
+                .typographyStyle(.heading2, with: .neutral950)
+            Text("트레이너와 트레이니 중 선택해주세요.")
+                .typographyStyle(.body1Medium, with: .neutral500)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal, 24)
+    }
+    
+    var ImageSection: some View {
+        Group {
+            if store.userType == .trainer {
+                Image(.imgOnboardingTrainer)
+                    .resizable()
+            } else {
+                Image(.imgOnboardingTrainee)
+                    .resizable()
+            }
+        }
+        .frame(idealWidth: 310, idealHeight: 310)
+        .padding(.horizontal, 20)
+    }
+    
+    var ButtonSection: some View {
+        HStack(spacing: 8) {
+            TempButton(
+                isSelected: store.userType == UserType.trainer,
+                title: UserType.trainer.koreanName,
+                action: { send(.tapUserTypeButton(.trainer), animation: .easeInOut(duration: 0.5)) }
+            )
+            TempButton(
+                isSelected: store.userType == UserType.trainee,
+                title: UserType.trainee.koreanName,
+                action: { send(.tapUserTypeButton(.trainee), animation: .easeInOut(duration: 0.5)) }
+            )
+        }
+        .padding(.horizontal, 20)
+    }
+}
+
+// TODO: 버튼 컴포넌트 나오면 대체
+private extension UserTypeSelectionView {
+    struct TempButton: View {
+        var isSelected: Bool = false
+        var title: String
+        let action: (() -> Void)
+        
+        var body: some View {
+            Button(action: {
+                action()
+            }) {
+                Text(title)
+                    .typographyStyle(.body1Medium, with: isSelected ? .red600 : .neutral500)
+                    .padding(.vertical, 16)
+                    .frame(height: 58)
+                    .frame(maxWidth: .infinity)
+                    .background(isSelected ? Color.red50 : .clear)
+                    .cornerRadius(16)
+                    .overlay(
+                      RoundedRectangle(cornerRadius: 16)
+                        .stroke(isSelected ? Color.red400 : Color.neutral300, lineWidth: isSelected ? 1.5 : 1.0)
+                    )
+            }
+        }
+    }
+    
+    struct BottomTempButton: View {
+        let action: (() -> Void)
+        
+        var body: some View {
+            Button(action: {
+                action()
+            }) {
+                Text("다음")
+                    .typographyStyle(.heading4, with: .neutral50)
+                    .padding(.top, 20)
+                    .padding(.bottom, 53)
+                    .frame(height: 100)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.neutral900)
+            }
+        }
+    }
 }
