2cd27c13a2790d5b439a787580c740478762f566
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-01-22 04:58:46 +0900

    [Delete] 불필요 파일 삭제

--- TnT/Projects/Data/Sources/Foundation/NetworkError.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NetworkError.swift
-//  Data
-//
-//  Created by 박민서 on 1/21/25.
-//  Copyright © 2025 yapp25thTeamTnT. All rights reserved.
-//
-
-import Foundation
--- TnT/Projects/Data/Sources/Foundation/TargetType.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TargetType.swift
-//  Data
-//
-//  Created by 박민서 on 1/21/25.
-//  Copyright © 2025 yapp25thTeamTnT. All rights reserved.
-//
-
-import Foundation
