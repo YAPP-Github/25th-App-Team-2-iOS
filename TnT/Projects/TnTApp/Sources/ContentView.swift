9d619971fd202ccaae1e6a09e01b6612073a90b6
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-01-22 04:52:29 +0900

    [Chore] 불필요코드 수정

--- TnT/Projects/TnTApp/Sources/ContentView.swift
+++ TnT/Projects/TnTApp/Sources/ContentView.swift
@@ -12,35 +12,8 @@ import Presentation
 
 struct ContentView: View {
     var body: some View {
-        UserTypeSelectionView(store: .init(initialState: UserTypeSelectionFeature.State(), reducer: {
-            UserTypeSelectionFeature()
-        }))
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-    
-//    private func encodeParameters(request: URLRequest, parameters: [String: Any], encoding: ParameterEncoding) throws -> URLRequest {
-//        var request = request
-//        switch encoding {
-//        case .url:
-//            let queryItems = parameters.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
-//            var components = URLComponents(url: request.url!, resolvingAgainstBaseURL: false)
-//            components?.queryItems = queryItems
-//            request.url = components?.url
-//        case .json:
-//            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: [])
-//            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-//        }
-//        return request
-//    }
-//    
-//    private func createMultipartBody(data: Data, fileName: String, mimeType: String, boundary: String) -> Data {
-//        var body = Data()
-//        body.append("--\(boundary)\r\n".data(using: .utf8)!)
-//        body.append("Content-Disposition: form-data; name=\"file\"; filename=\"\(fileName)\"\r\n".data(using: .utf8)!)
-//        body.append("Content-Type: \(mimeType)\r\n\r\n".data(using: .utf8)!)
-//        body.append(data)
-//        body.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
-//        return body
-//    }
 }
 
 #Preview {
