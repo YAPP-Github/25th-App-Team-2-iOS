cae3b554a48d0f637c2909178b1732b4486efede
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-01-25 01:04:20 +0900

    [Feat] TraineeBasicInfoInput 화면 작성

--- TnT/Projects/DesignSystem/Sources/Components/DatePickerView/TDatePickerView.swift
+++ TnT/Projects/DesignSystem/Sources/Components/DatePickerView/TDatePickerView.swift
@@ -6,4 +6,53 @@
 //  Copyright © 2025 yapp25thTeamTnT. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+/// - 날짜 선택할 수 있는 DatePicker View
+public struct TDatePickerView: View {
+    
+    /// picker 제목
+    private let title: String
+    /// 버튼 실행 action
+    private let buttonAction: (Date) -> Void
+    /// 선택 날짜
+    @State private var selectedDate: Date = .now
+    
+    /// `TDatePickerView` 생성자
+    /// - Parameters:
+    ///   - selectedDate: 초기 선택 날짜 (기본값: 현재 날짜)
+    ///   - title: DatePicker의 제목
+    ///   - buttonAction: 날짜 선택 후 실행할 액션
+    public init(
+        selectedDate: Date = .now,
+        title: String,
+        buttonAction: @escaping (Date) -> Void
+    ) {
+        self.selectedDate = selectedDate
+        self.title = title
+        self.buttonAction = buttonAction
+    }
+    
+    public var body: some View {
+        VStack {
+            DatePicker(
+                title,
+                selection: $selectedDate,
+                displayedComponents: [.date]
+            )
+            .tint(Color.red500)
+            .datePickerStyle(.graphical)
+            .labelsHidden()
+            .padding()
+            
+            TBottomButton(
+                title: "완료",
+                state: .true,
+                action: {
+                    buttonAction(selectedDate)
+                }
+            )
+            .ignoresSafeArea(.all, edges: .bottom)
+        }
+    }
+}
--- TnT/Projects/Presentation/Sources/Onboarding/Trainee/TraineeBasicInfoInput/TraineeBasicInfoInputView.swift
+++ TnT/Projects/Presentation/Sources/Onboarding/Trainee/TraineeBasicInfoInput/TraineeBasicInfoInputView.swift
@@ -9,15 +9,24 @@
 import SwiftUI
 import ComposableArchitecture
 
+import Domain
 import DesignSystem
 
+/// 회원 기본 정보를 입력하는 화면
+/// - 생년월일, 키, 체중 입력을 포함
+/// - 생년월일 입력 시 DatePicker 표시
+@ViewAction(for: TraineeBasicInfoInputFeature.self)
 public struct TraineeBasicInfoInputView: View {
     
-//    public var store: StoreOf
-    
+    @Bindable public var store: StoreOf<TraineeBasicInfoInputFeature>
+    @FocusState private var focusedField: Field?
     @Environment(\.dismiss) var dismiss: DismissAction
     
-    public init() {}
+    /// `TraineeBasicInfoInputView` 생성자
+    /// - Parameter store: `TraineeBasicInfoInputFeature`와 연결된 Store
+    public init(store: StoreOf<TraineeBasicInfoInputFeature>) {
+        self.store = store
+    }
     
     public var body: some View {
         VStack(spacing: 0) {
@@ -29,26 +38,49 @@ public struct TraineeBasicInfoInputView: View {
             TextFieldSection
             
             Spacer()
-            
-            TBottomButton(
-                title: "다음",
-                state: .true
-            ) {
-                print("asdf")
-            }
-            .ignoresSafeArea(.container, edges: .bottom)
         }
         .navigationBarBackButtonHidden()
+        .keyboardDismissOnTap()
+        .safeAreaInset(edge: .bottom) {
+            if store.view_focusField == nil {
+                TBottomButton(
+                    title: "다음",
+                    state: store.view_isNextButtonEnabled ? .true : .false
+                ) {
+                    print("asdf")
+                }
+            }
+        }
+        .sheet(isPresented: $store.view_isDatePickerPresented) {
+            TDatePickerView(title: "생년월일") { date in
+                send(.tapBirthDatePickerDoneButton(date))
+            }
+            .presentationDetents([.medium])
+            .interactiveDismissDisabled(true)
+        }
+        .onChange(of: focusedField) { oldValue, newValue in
+            if oldValue != newValue {
+                send(.setFocus(oldValue, newValue))
+            }
+        }
+    }
+}
+
+public extension TraineeBasicInfoInputView {
+    enum Field: Sendable, Hashable {
+        case birthDate
+        case height
+        case weight
     }
 }
 
 private extension TraineeBasicInfoInputView {
     var Header: some View {
         VStack(spacing: 12) {
-            // PageIndicatorList
+            // 페이지 인디케이터
             HStack(spacing: 4) {
                 ForEach(1...4, id: \.self) { num in
-                    TPageIndicator(pageNumber: num, isCurrent: true)
+                    TPageIndicator(pageNumber: num, isCurrent: num == 1)
                 }
                 Spacer()
             }
@@ -64,35 +96,47 @@ private extension TraineeBasicInfoInputView {
         VStack(spacing: 48) {
             TTextField(
                 placeholder: "2000/01/01",
-                text: .constant(""),
-                textFieldStatus: .constant(.empty)
+                text: $store.birthDate,
+                textFieldStatus: $store.view_birthDateStatus
             )
             .withSectionLayout(header: .init(isRequired: false, title: "생년월일", limitCount: nil, textCount: nil))
-            .keyboardType(.numberPad)
+            .focused($focusedField, equals: .birthDate)
+            .disabled(true)
+            .onTapGesture {
+                send(.tapBirthDateTextField)
+            }
             .frame(maxWidth: .infinity)
             .padding(.horizontal, 20)
             
             HStack(spacing: 12) {
                 TTextField(
                     placeholder: "0",
-                    text: .constant(""),
-                    textFieldStatus: .constant(.empty),
+                    text: $store.height,
+                    textFieldStatus: $store.view_heightStatus,
                     rightView: {
-                        TTextField.RightView(style: .unit(text: "cm", status: .filled))
+                        TTextField.RightView(style: .unit(text: UnitText.height.koreaUnit, status: .filled))
                     }
                 )
-                .withSectionLayout(header: .init(isRequired: true, title: "키", limitCount: nil, textCount: nil))
+                .withSectionLayout(
+                    header: .init(isRequired: true, title: "키", limitCount: nil, textCount: nil),
+                    footer: .init(footerText: "잘못된 수치를 입력했어요", status: store.view_heightStatus)
+                )
+                .focused($focusedField, equals: .height)
                 .keyboardType(.numberPad)
                 
                 TTextField(
                     placeholder: "00.0",
-                    text: .constant(""),
-                    textFieldStatus: .constant(.empty),
+                    text: $store.weight,
+                    textFieldStatus: $store.view_weightStatus,
                     rightView: {
-                        TTextField.RightView(style: .unit(text: "kg", status: .filled))
+                        TTextField.RightView(style: .unit(text: UnitText.weight.koreaUnit, status: .filled))
                     }
                 )
-                .withSectionLayout(header: .init(isRequired: true, title: "체중", limitCount: nil, textCount: nil))
+                .withSectionLayout(
+                    header: .init(isRequired: true, title: "체중", limitCount: nil, textCount: nil),
+                    footer: .init(footerText: "잘못된 수치를 입력했어요", status: store.view_weightStatus)
+                )
+                .focused($focusedField, equals: .weight)
                 .keyboardType(.decimalPad)
             }
             .padding(.horizontal, 20)
