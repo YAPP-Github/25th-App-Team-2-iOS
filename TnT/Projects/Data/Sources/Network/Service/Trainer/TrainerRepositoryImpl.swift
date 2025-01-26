8f3e4c271158353363d626113135e77840fbefa7
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-01-27 05:01:44 +0900

    [Feat] TrainerRepository 작성

--- TnT/Projects/Data/Sources/Network/Service/Trainer/TrainerRepositoryImpl.swift
+++ TnT/Projects/Data/Sources/Network/Service/Trainer/TrainerRepositoryImpl.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import Dependencies
+
+import Domain
+
+/// 트레이너 관련 네트워크 요청을 처리하는 TrainerRepository 구현체
+public struct TrainerRepositoryImpl: TrainerRepository {
+    private let networkService: NetworkService = .shared
+    
+    public init() {}
+    
+    public func getVerifyInvitationCode(code: String) async throws -> GetVerifyInvitationCodeResDTO {
+        return try await networkService.request(
+            TrainerTargetType.getVerifyInvitationCode(code: code),
+            decodingType: GetVerifyInvitationCodeResDTO.self
+        )
+    }
+}
