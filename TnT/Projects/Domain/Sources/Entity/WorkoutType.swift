4c8958a61608f2368e6d53073942b7eb03d94d31
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-01-29 02:48:11 +0900

    [Feat] Presentation단 도메인 UI 표시 extension 작성

--- TnT/Projects/Domain/Sources/Entity/WorkoutType.swift
+++ TnT/Projects/Domain/Sources/Entity/WorkoutType.swift
@@ -1,5 +1,5 @@
 //
-//  WrokoutType.swift
+//  WorkoutType.swift
 //  Domain
 //
 //  Created by 박민서 on 1/29/25.
---
+++ TnT/Projects/Presentation/Sources/PresentationSupport/MealType.swift
@@ -0,0 +1,26 @@
+//
+//  MealType.swift
+//  Presentation
+//
+//  Created by 박민서 on 1/29/25.
+//  Copyright © 2025 yapp25thTeamTnT. All rights reserved.
+//
+
+import Domain
+import DesignSystem
+
+extension MealType {
+    /// 해당 식단을 표시하는 이모지 입니다
+    var emoji: String {
+        switch self {
+        case .morning:
+            "🌞"
+        case .lunch:
+            "⛅"
+        case .dinner:
+            "🌙"
+        case .snack:
+            "🍰"
+        }
+    }
+}
