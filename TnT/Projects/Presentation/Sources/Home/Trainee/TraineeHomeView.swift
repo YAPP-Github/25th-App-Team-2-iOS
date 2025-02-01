25c663ce4542e48e4260278533fad155f8e64485
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-02-02 03:32:21 +0900

    [Feat[ AutoResizingBottomSheet Modifier 작성

---
+++ TnT/Projects/Presentation/Sources/Home/Trainee/TraineeHomeView.swift
@@ -0,0 +1,9 @@
+//
+//  TraineeHomeView.swift
+//  Presentation
+//
+//  Created by 박민서 on 2/2/25.
+//  Copyright © 2025 yapp25thTeamTnT. All rights reserved.
+//
+
+import Foundation
---
+++ TnT/Projects/Presentation/Sources/Home/Trainee/TraineeRecordStartView.swift
@@ -0,0 +1,9 @@
+//
+//  TraineeRecordStartVeiw.swift
+//  Presentation
+//
+//  Created by 박민서 on 2/2/25.
+//  Copyright © 2025 yapp25thTeamTnT. All rights reserved.
+//
+
+import Foundation
