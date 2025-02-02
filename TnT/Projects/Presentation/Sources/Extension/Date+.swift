a3ab3e727b526f6cad2078f4d9ebb275cdf1fe7d
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-02-02 21:36:13 +0900

    [Feat] Presentation 시간 표시용 extension 추가

--- TnT/Projects/Presentation/Sources/Extension/Date+.swift
+++ TnT/Projects/Presentation/Sources/Extension/Date+.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+import Domain
+
+extension Date {
+    func timeAgoDisplay() -> String {
+        let now = Date()
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.second, .minute, .hour, .day], from: self, to: now)
+        
+        if let day = components.day, day >= 1 {
+            return TDateFormatUtility.formatter(for: .M월_d일).string(from: self)
+        } else if let hour = components.hour, hour >= 1 {
+            return "\(hour)시간 전"
+        } else if let minute = components.minute, minute >= 1 {
+            return "\(minute)분 전"
+        } else {
+            return "방금"
+        }
+    }
+}
