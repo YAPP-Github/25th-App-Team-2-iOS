24e4b92e56e2d43c17d3f178331721830dd39c6c
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-02-03 21:26:54 +0900

    [Feat] TraineeInvitationCodeInput 팝업, 네비 분기처리 추가

--- TnT/Projects/Presentation/Sources/Onboarding/Trainee/TraineeInvitationCodeInput/TraineeInvitationCodeInputFeature.swift
+++ TnT/Projects/Presentation/Sources/Onboarding/Trainee/TraineeInvitationCodeInput/TraineeInvitationCodeInputFeature.swift
@@ -21,6 +21,8 @@ public struct TraineeInvitationCodeInputFeature {
         // MARK: Data related state
         /// 입력된 초대코드
         var invitationCode: String
+        /// 표시되는 팝업
+        var presentPopUp: PopUp?
         
         // MARK: UI related state
         /// 텍스트 필드 상태 (빈 값 / 입력됨 / 유효하지 않음)
@@ -35,25 +37,41 @@ public struct TraineeInvitationCodeInputFeature {
         var view_isNextButtonEnabled: Bool
         /// 팝업 표시 여부
         var view_isPopupPresented: Bool
+        /// 상단 네비바 표시 상태
+        var view_navigationType: NavigationType
+        
         
         /// `TraineeInvitationCodeInputFeature.State`의 생성자
         /// - Parameters:
+        ///   - invitationCode: 사용자가 입력한 초대 코드 (기본값: `""`)
+        ///   - presentPopUp: 현재 표시되는 팝업 (기본값: `nil`)
+        ///   - view_invitationCodeStatus: 텍스트 필드 상태 (`.empty`, `.valid`, `.invalid` 등)
+        ///   - view_textFieldFooterText: 텍스트 필드 하단에 표시될 메시지 (기본값: `""`)
+        ///   - view_isFieldFocused: 현재 텍스트 필드가 포커스를 받고 있는지 여부 (기본값: `false`)
+        ///   - view_isVerityButtonEnabled: "인증하기" 버튼 활성화 여부 (기본값: `false`)
+        ///   - view_isNextButtonEnabled: "다음" 버튼 활성화 여부 (기본값: `false`)
+        ///   - view_isPopupPresented: 팝업이 표시 중인지 여부 (기본값: `false`)
+        ///   - view_navigationType: 현재 화면의 네비게이션 타입 (`.newUser`: "건너뛰기" 버튼 있음, `.existingUser`: "뒤로가기" 버튼 있음)
         public init(
             invitationCode: String = "",
+            presentPopUp: PopUp? = nil,
             view_invitationCodeStatus: TTextField.Status = .empty,
             view_textFieldFooterText: String = "",
             view_isFieldFocused: Bool = false,
             view_isVerityButtonEnabled: Bool = false,
             view_isNextButtonEnabled: Bool = false,
-            view_isPopupPresented: Bool = true
+            view_isPopupPresented: Bool = false,
+            view_navigationType: NavigationType = .newUser
         ) {
             self.invitationCode = invitationCode
+            self.presentPopUp = presentPopUp
             self.view_invitationCodeStatus = view_invitationCodeStatus
             self.view_textFieldFooterText = view_textFieldFooterText
             self.view_isFieldFocused = view_isFieldFocused
             self.view_isVerityButtonEnabled = view_isVerityButtonEnabled
             self.view_isNextButtonEnabled = view_isNextButtonEnabled
             self.view_isPopupPresented = view_isPopupPresented
+            self.view_navigationType = view_navigationType
         }
     }
     
@@ -79,10 +97,16 @@ public struct TraineeInvitationCodeInputFeature {
             case setFocus(Bool)
             /// Nav바 건너뛰기 버튼이 눌렸을 때
             case tapNavBarSkipButton
+            /// Nav바 back 버튼이 눌렸을 때
+            case tapNavBarBackButton
             /// 팝업 "다음에 할게요" 버튼이 눌렸을 때
-            case tapPopupNextButton
+            case tapInvitePopupNextButton
             /// 팝업 "확인" 버튼이 눌렸을 때
-            case tapPopupConfirmButton
+            case tapInvitePopupConfirmButton
+            /// 팝업 "중단하기" 버튼이 눌렸을 때
+            case tapDropAlertStopButton
+            /// 팝업 "계속 진행" 버튼이 눌렸을 때
+            case tapDropAlertKeepButton
         }
     }
     
@@ -100,7 +124,7 @@ public struct TraineeInvitationCodeInputFeature {
                     
                 case .binding:
                     return .none
-
+                    
                 case .tapVerifyButton:
                     return .run { [state] send in
                         let result: Bool = try await traineeUseCase.verifyTrainerInvitationCode(state.invitationCode)
@@ -109,20 +133,22 @@ public struct TraineeInvitationCodeInputFeature {
                     
                 case .tapNextButton:
                     return .send(.setNavigating(.trainingInfoInput))
-                
+                    
                 case .setFocus(let isFocused):
                     state.view_isFieldFocused = isFocused
                     return .none
-                
-                case .tapNavBarSkipButton:
+                    
+                case .tapNavBarSkipButton, .tapNavBarBackButton:
+                    // 인증 후에만 팝업 표시
+                    return state.view_invitationCodeStatus == .valid
+                    ? self.setPopUpStatus(&state, status: .dropAlert)
+                    : .send(.setNavigating(.traineeHome))
+                    
+                case .tapInvitePopupNextButton, .tapDropAlertStopButton:
                     return .send(.setNavigating(.traineeHome))
                     
-                case .tapPopupNextButton:
-                    return .send(.setNavigating(.traineeHome))
-                    
-                case .tapPopupConfirmButton:
-                    state.view_isPopupPresented = false
-                    return .none
+                case .tapDropAlertKeepButton, .tapInvitePopupConfirmButton:
+                    return self.setPopUpStatus(&state, status: nil)
                 }
                 
             case .updateVerificationStatus(let isVerified):
@@ -130,7 +156,7 @@ public struct TraineeInvitationCodeInputFeature {
                 state.view_invitationCodeStatus = isVerified ? .valid : .invalid
                 state.view_isNextButtonEnabled = isVerified
                 return .none
-            
+                
             case .setNavigating:
                 return .none
             }
@@ -160,11 +186,35 @@ private extension TraineeInvitationCodeInputFeature {
         
         return .none
     }
+    
+    /// 팝업 상태, 표시 상태를 업데이트
+    func setPopUpStatus(_ state: inout State, status: PopUp?) -> Effect<Action> {
+        state.presentPopUp = status
+        state.view_isPopupPresented = status != nil
+        return .none
+    }
 }
 
 public extension TraineeInvitationCodeInputFeature {
+    /// 본 화면에서 라우팅(파생)되는 화면
     enum RoutingScreen: Sendable {
         case traineeHome
         case trainingInfoInput
+    }
+    
+    /// 본 화면에 팝업으로 표시되는 목록
+    enum PopUp: Sendable {
+        /// 진입 시 초대 코드를 입력해주세요
+        case invitePopUp
+        /// 연결을 중단하시겠어요?
+        case dropAlert
+    }
+    
+    /// 본 화면의 네비게이션 타입
+    enum NavigationType: Equatable {
+        /// 신규 유저 (우측 건너뛰기 버튼)
+        case newUser
+        /// 기존 유저 (좌측 뒤로가기 버튼)
+        case existingUser
     }
 }
--- TnT/Projects/Presentation/Sources/Onboarding/Trainee/TraineeInvitationCodeInput/TraineeInvitationCodeInputView.swift
+++ TnT/Projects/Presentation/Sources/Onboarding/Trainee/TraineeInvitationCodeInput/TraineeInvitationCodeInputView.swift
@@ -27,15 +27,7 @@ public struct TraineeInvitationCodeInputView: View {
     
     public var body: some View {
         VStack(spacing: 0) {
-            TNavigation(
-                type: .RTextWithTitle(
-                    centerTitle: "연결하기",
-                    rightText: "건너뛰기"
-                ),
-                rightAction: {
-                    send(.tapNavBarSkipButton)
-                }
-            )
+            NavigationBar()
             .padding(.bottom, 24)
             
             Header()
@@ -63,18 +55,46 @@ public struct TraineeInvitationCodeInputView: View {
             }
         }
         .tPopUp(isPresented: $store.view_isPopupPresented) {
-            PopUpView(
-                secondaryAction: {
-                    send(.tapPopupNextButton)
-                },
-                primaryAction: {
-                    send(.tapPopupConfirmButton)
+            guard let popUp = store.presentPopUp else {
+                return TPopUpAlertView(alertState: .init(title: "Error"))
+            }
+            switch popUp {
+            case .invitePopUp:
+                return TrainerInvitePopup()
+                
+            case .dropAlert:
+                return DropAlertPopup()
+            }
+        }
+    }
+    
+    // MARK: - Sections
+    @ViewBuilder
+    private func NavigationBar() -> some View {
+        switch store.view_navigationType {
+        case .newUser:
+            TNavigation(
+                type: .RTextWithTitle(
+                    centerTitle: "연결하기",
+                    rightText: "건너뛰기"
+                ),
+                rightAction: {
+                    send(.tapNavBarSkipButton)
+                }
+            )
+        case .existingUser:
+            TNavigation(
+                type: .LButtonWithTitle(
+                    leftImage: .icnArrowLeft,
+                    centerTitle: "연결하기"
+                ),
+                leftAction: {
+                    send(.tapNavBarBackButton)
                 }
             )
         }
     }
     
-    // MARK: - Sections
     @ViewBuilder
     private func Header() -> some View {
         TInfoTitleHeader(title: "트레이너에게 받은\n초대 코드를 입력해 주세요")
@@ -113,42 +133,58 @@ public struct TraineeInvitationCodeInputView: View {
 }
 
 private extension TraineeInvitationCodeInputView {
-    
-    struct PopUpView: View {
-        let secondaryAction: () -> Void
-        let primaryAction: () -> Void
-        
-        init(
-            secondaryAction: @escaping () -> Void,
-            primaryAction: @escaping () -> Void
-        ) {
-            self.secondaryAction = secondaryAction
-            self.primaryAction = primaryAction
-        }
-        
-        var body: some View {
-            TPopUpAlertView(
-                alertState: .init(
-                    title: "트레이너에게 받은\n초대 코드를 입력해보세요!",
-                    message: "트레이너와 연결하지 않을 경우\n일부 기능이 제한될 수 있어요.",
-                    buttons: [
-                        .init(
-                            title: "다음에 할게요",
-                            style: .secondary,
-                            action: .init(action: {
-                                secondaryAction()
-                            })
-                        ),
-                        .init(
-                            title: "확인",
-                            style: .primary,
-                            action: .init(action: {
-                                primaryAction()
-                            })
-                        )
-                    ]
-                )
+    @ViewBuilder
+    /// 진입 시 트레이너 초대 코드 권유 팝업
+    private func TrainerInvitePopup() -> TPopUpAlertView {
+        TPopUpAlertView(
+            alertState: .init(
+                title: "트레이너에게 받은\n초대 코드를 입력해보세요!",
+                message: "트레이너와 연결하지 않을 경우\n일부 기능이 제한될 수 있어요.",
+                buttons: [
+                    .init(
+                        title: "다음에 할게요",
+                        style: .secondary,
+                        action: .init(action: {
+                            send(.tapInvitePopupNextButton)
+                        })
+                    ),
+                    .init(
+                        title: "확인",
+                        style: .primary,
+                        action: .init(action: {
+                            send(.tapInvitePopupConfirmButton)
+                        })
+                    )
+                ]
             )
-        }
+        )
+    }
+    
+    @ViewBuilder
+    /// 코드 인증 후 화면을 벗어나려는 경우 팝업
+    private func DropAlertPopup() -> TPopUpAlertView {
+        TPopUpAlertView(
+            alertState: .init(
+                title: "트레이너 연결을 중단하시겠어요?",
+                message: "중단 시 연결을 처음부터 다시 설정해야 해요",
+                showAlertIcon: true,
+                buttons: [
+                    .init(
+                        title: "중단하기",
+                        style: .secondary,
+                        action: .init(action: {
+                            send(.tapDropAlertStopButton)
+                        })
+                    ),
+                    .init(
+                        title: "계속 진행",
+                        style: .primary,
+                        action: .init(action: {
+                            send(.tapDropAlertKeepButton)
+                        })
+                    )
+                ]
+            )
+        )
     }
 }
