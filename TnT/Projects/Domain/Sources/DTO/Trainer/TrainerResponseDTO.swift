765785fbb18dd6f0168aeffc7fe15d1e33c5ef3d
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-02-11 02:46:10 +0900

    [Feat] TraineeInvitationCodeInput API 연결

--- TnT/Projects/Domain/Sources/DTO/Trainer/TrainerResponseDTO.swift
+++ TnT/Projects/Domain/Sources/DTO/Trainer/TrainerResponseDTO.swift
@@ -12,4 +12,6 @@ import Foundation
 public struct GetVerifyInvitationCodeResDTO: Decodable {
     /// 초대 코드 인증 여부
     public let isVerified: Bool
+    /// 트레이너 이름
+    public let trainerName: String?
 }
