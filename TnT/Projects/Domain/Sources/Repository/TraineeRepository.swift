26d8f1e53726cc7bd78e1230e7aa18f372fa929a
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-02-12 00:50:44 +0900

    [Feat] Trainee Repo, usecas 추가

--- TnT/Projects/Domain/Sources/Repository/TraineeRepository.swift
+++ TnT/Projects/Domain/Sources/Repository/TraineeRepository.swift
@@ -16,4 +16,12 @@ public protocol TraineeRepository {
     /// - Returns: 연결 성공 시, 연결된 트레이너 정보가 포함된 응답 DTO (`PostConnectTrainerResDTO`)
     /// - Throws: 네트워크 오류 또는 잘못된 요청 데이터로 인한 서버 오류 발생 가능
     func postConnectTrainer(_ reqDTO: PostConnectTrainerReqDTO) async throws -> PostConnectTrainerResDTO
+    
+    /// 회원가입 요청
+    /// - Parameters:
+    ///   - reqDTO: 식단 등록 요청에 필요한 데이터
+    ///   - imgData: 사용자가 업로드한 식단 이미지 (옵션)
+    /// - Returns: 등록 성공 시, 응답 DTO (empty) (`PostTraineeDietRecordResDTO`)
+    /// - Throws: 네트워크 오류 또는 서버에서 반환한 오류를 발생시킬 수 있음
+    func postTraineeDietRecord(_ reqDTO: PostTraineeDietRecordReqDTO, imgData: Data?) async throws -> PostTraineeDietRecordResDTO
 }
