63ad9a70ea4f572873a0b2bebb81b9a65336d449
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-02-12 20:50:37 +0900

    [Feat] Data - Trainer API 최신화 작성

--- TnT/Projects/Data/Sources/Network/Service/Trainer/TrainerTargetType.swift
+++ TnT/Projects/Data/Sources/Network/Service/Trainer/TrainerTargetType.swift
@@ -18,12 +18,20 @@ public enum TrainerTargetType {
     case getFirstInvitationCode
     /// 트레이너 캘린더, 특정 날짜의 PT 리스트 불러오기
     case getDateLessonList(date: String)
+    /// 달력 스케줄 카운트 표시에 필요한 PT 리스트 불러오기
+    case getMonthlyLessonList(year: Int, month: Int)
     /// 트레이너 초대코드 재발급
-    case getReissuanceInvitationCode
+    case putReissuanceInvitationCode
     /// 회원 조희
     case getMemebersList
     /// 연결 완료된 트레이니 최초로 정보 불러오기
     case getConnectedTraineeInfo(trainerId: Int, traineeId: Int)
+    /// 관리 중인 회원 목록 요청
+    case getActiveTraineesList
+    /// PT 수업 추가
+    case postLesson(reqDTO: PostLessonReqDTO)
+    /// PT 수업 완료 처리
+    case putCompleteLesson(lessonId: Int)
 }
 
 extension TrainerTargetType: TargetType {
@@ -40,65 +48,64 @@ extension TrainerTargetType: TargetType {
             return "/invitation-code"
         case .getDateLessonList(let date):
             return "/lessons/\(date)"
-        case .getReissuanceInvitationCode:
+        case .getMonthlyLessonList:
+            return "/lessons/calendar"
+        case .putReissuanceInvitationCode:
             return "/invitation-code/reissue"
         case .getMemebersList:
             return "/members"
         case .getConnectedTraineeInfo:
             return "/first-connected-trainee"
+        case .getActiveTraineesList:
+            return "/active-trainees"
+        case .postLesson:
+            return "lessons"
+        case .putCompleteLesson(let lessonId):
+            return "lessons/\(lessonId)/complete"
         }
     }
     
     var method: HTTPMethod {
         switch self {
-        case .getVerifyInvitationCode:
+        case .getVerifyInvitationCode, .getFirstInvitationCode, .getDateLessonList, .getMemebersList, .getConnectedTraineeInfo, .getMonthlyLessonList, .getActiveTraineesList:
             return .get
-        case .getFirstInvitationCode:
-            return .get
-        case .getDateLessonList:
-            return .get
-        case .getReissuanceInvitationCode:
+            
+        case .putReissuanceInvitationCode, .putCompleteLesson:
             return .put
-        case .getMemebersList:
-            return .get
-        case .getConnectedTraineeInfo:
-            return .get
+            
+        case .postLesson:
+            return .post
         }
     }
     
     var task: RequestTask {
         switch self {
-        case .getVerifyInvitationCode:
+        case .getVerifyInvitationCode, .getFirstInvitationCode, .getDateLessonList, .putReissuanceInvitationCode, .getMemebersList, .getActiveTraineesList, .putCompleteLesson:
             return .requestPlain
-        case .getFirstInvitationCode:
-            return .requestPlain
-        case .getDateLessonList:
-            return .requestPlain
-        case .getReissuanceInvitationCode:
-            return .requestPlain
-        case .getMemebersList:
-            return .requestPlain
+            
+        case let .getMonthlyLessonList(year, month):
+            return .requestParameters(parameters: [
+                "year": year,
+                "month": month
+            ], encoding: .url)
+            
         case let .getConnectedTraineeInfo(trainerId, traineeId):
             return .requestParameters(parameters: [
                 "trainerId": trainerId,
                 "traineeId": traineeId
             ], encoding: .url)
+            
+        case .postLesson(let reqDTO):
+            return .requestJSONEncodable(encodable: reqDTO)
         }
     }
     
     var headers: [String: String]? {
         switch self {
-        case .getVerifyInvitationCode:
+        case .getVerifyInvitationCode, .getDateLessonList, .putReissuanceInvitationCode, .getMemebersList, .getMonthlyLessonList, .postLesson, .getActiveTraineesList:
             return ["Content-Type": "application/json"]
-        case .getFirstInvitationCode:
-            return nil
-        case .getDateLessonList:
-            return ["Content-Type": "application/json"]
-        case .getReissuanceInvitationCode:
-            return ["Content-Type": "application/json"]
-        case .getMemebersList:
-            return ["Content-Type": "application/json"]
-        case .getConnectedTraineeInfo:
+            
+        case .getFirstInvitationCode, .getConnectedTraineeInfo, .putCompleteLesson:
             return nil
         }
     }
--- TnT/Projects/Domain/Sources/DTO/Trainer/TrainerResponseDTO.swift
+++ TnT/Projects/Domain/Sources/DTO/Trainer/TrainerResponseDTO.swift
@@ -15,3 +15,36 @@ public struct GetVerifyInvitationCodeResDTO: Decodable {
     /// 트레이너 이름
     public let trainerName: String?
 }
+
+/// 달력 스케줄 카운트 표시에 필요한 PT 리스트 불러오기 응답 DTO
+public struct GetMonthlyLessonListResDTO: Decodable {
+    public let calendarPtLessonCounts: [DailyLessonCountResDTO]
+}
+
+/// 일별 스케줄 카운트 표시용 응답 DTO
+public struct DailyLessonCountResDTO: Decodable {
+    public let date: String
+    public let count: Int
+}
+
+/// 관리 중인 회원 목록 응답 DTO
+public struct GetActiveTraineesListResDTO: Decodable {
+    public let trainees: [ActiveTraineeInfoResDTO]
+}
+
+/// 관리 중인 회원 정보 DTO
+public struct ActiveTraineeInfoResDTO: Decodable {
+    public let id: Int
+    public let name: String
+    public let profileImageUrl: String
+    public let finishedPtCount: Int
+    public let totalPtCount: Int
+    public let memo: String
+    public let ptGoals: [String]
+}
+
+/// PT 수업 추가 응답 DTO
+public typealias PostLessonResDTO = EmptyResponse
+
+/// PT 수업 완료 처리 응답 DTO
+public typealias PutCompleteLessonResDTO = EmptyResponse
