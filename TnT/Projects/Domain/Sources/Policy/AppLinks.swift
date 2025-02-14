ee21d11db46a6ffbe330f79ed38821f61404e358
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-02-14 18:08:14 +0900

    [Feat] AppLinks 업데이트

--- TnT/Projects/Domain/Sources/Policy/AppLinks.swift
+++ TnT/Projects/Domain/Sources/Policy/AppLinks.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public enum AppLinks {
-    public static let termsOfService: String = "https://yapp25thteam.com/terms"
-    public static let privacyPolicy: String = "https://yapp25thteam.com/privacy"
-    public static let openSourceLicense: String = "https://yapp25thteam.com/license"
+    public static let termsOfService: String = "https://www.notion.so/f1ee7a43b6d941068723163fda127699?pvs=4"
+    public static let privacyPolicy: String = "https://www.notion.so/775bc037dd1b4e8ba56679e51a7321e5?pvs=4"
+    public static let openSourceLicense: String = "https://www.notion.so/197cabde9e4b800d8e8ec6c22d343b32?pvs=4"
 }
